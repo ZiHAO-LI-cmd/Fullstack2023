dfc4b5f380de9d9b34bd1b86864d2eb325d509b1
zihao <zihao-lee@outlook.com>
2023-11-01 00:36:15 +0200

    -3.11

--- part2/phonebook/src/services/Phonebook.jsx
+++ part2/phonebook/src/services/Phonebook.jsx
@@ -1,5 +1,15 @@
+/*
+ * @Author: zihao zihao-lee@outlook.com
+ * @Date: 2023-10-10 11:10:53
+ * @LastEditors: zihao zihao-lee@outlook.com
+ * @LastEditTime: 2023-11-01 00:31:39
+ * @FilePath: \Fullstack2023\part2\phonebook\src\services\Phonebook.jsx
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by zihao, All Rights Reserved. 
+ */
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   return axios.get(baseUrl);
--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(express.static('dist'))
 
 let persons = [
   {
@@ -85,7 +88,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
