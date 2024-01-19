3adc8a42f22fde16415d02b561542137583daa21
zihao <zihao-lee@outlook.com>
2024-01-19 13:58:01 +0200

    -6.8

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,25 @@
+/*
+ * @Author: zihao zihao-lee@outlook.com
+ * @Date: 2024-01-19 12:34:23
+ * @LastEditors: zihao zihao-lee@outlook.com
+ * @LastEditTime: 2024-01-19 13:52:10
+ * @FilePath: \Fullstack2023\part6\redux-anecdotes\src\App.jsx
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by zihao, All Rights Reserved. 
+ */
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteList></AnecdoteList>
+      <h2>create new</h2>
+      <AnecdoteForm></AnecdoteForm>
+    </div>
+  );
+};
+
+export default App;
